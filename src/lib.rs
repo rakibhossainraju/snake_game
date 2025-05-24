9bada7cbf946d2ebf354b82c7a13be3d52724b69
rakibhossainraju <rakibhossianraju@gmail.com>
2025-05-24 20:22:44 +0600

    Add Snake struct and integrate with World
    Introduced a new `Snake` struct with an initial body containing a spawn index. Updated the `World` struct to include a `Snake` instance and added a method to retrieve the snake's head position.

--- src/lib.rs
+++ src/lib.rs
@@ -11,18 +11,39 @@ extern "C" {
     pub fn error(s: &str);
 }
 
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+}
+impl Snake {
+    pub fn new(spawn_index: usize) -> Self {
+        Snake {
+            body: vec![SnakeCell(spawn_index)],
+        }
+    }
+}
+
 #[wasm_bindgen]
 struct World {
     width: usize,
+    snake: Snake,
 }
 
 #[wasm_bindgen]
 impl World {
-    pub fn new() -> World {
-        World { width: 8 }
+    pub fn new() -> Self {
+        World {
+            width: 8,
+            snake: Snake::new(10),
+        }
     }
 
     pub fn get_width(&self) -> usize {
         self.width
+    }
+
+    pub fn get_snake_head(&self) -> usize {
+        self.snake.body[0].0
     }
 }
