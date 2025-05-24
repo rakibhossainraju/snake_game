0c30254cef00366162f0ff30f0d998a45e7908ea
rakibhossainraju <rakibhossianraju@gmail.com>
2025-05-24 13:11:03 +0600

    Update styling and add grid rendering to game canvas
    Centered content wrapper styling in CSS and introduced dynamic grid rendering in the game canvas. The grid adapts to the world's size using a fixed cell size for improved visualization.

--- view/src/main.js
+++ view/src/main.js
@@ -1,6 +1,30 @@
-import {World} from "snake_game";
+import { World } from "snake_game";
+
+const CELL_SIZE = 25;
 
 const world = World.new();
+const worldSize = world.get_width();
 
 const gameCanvas = document.getElementById("snake-canvas");
 const ctx = gameCanvas.getContext("2d");
+
+gameCanvas.height = CELL_SIZE * worldSize;
+gameCanvas.width = CELL_SIZE * worldSize;
+
+drawWorld();
+
+function drawWorld() {
+  ctx.beginPath();
+
+  for (let line = 0; line <= worldSize; line++) {
+    // Draw Horizontal Lines
+    ctx.moveTo(CELL_SIZE * line, 0);
+    ctx.lineTo(CELL_SIZE * line, worldSize * CELL_SIZE);
+
+    // Draw Vertical Lines
+    ctx.moveTo(0, CELL_SIZE * line);
+    ctx.lineTo(CELL_SIZE * worldSize, CELL_SIZE * line);
+  }
+
+  ctx.stroke();
+}
