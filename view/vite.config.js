29a50a05ddb7ff13c74f54eda8a7345dc3f99bab
rakibhossainraju <rakibhossianraju@gmail.com>
2025-05-25 23:21:22 +0600

    Switch to TypeScript for frontend build process
    Replaced JavaScript bootstrap file with TypeScript equivalent and added TypeScript configurations and dependencies. Updated scripts in `package.json` to include TypeScript type-checking during builds. Enhanced consistency and future scalability of the codebase.

--- view/vite.config.js
+++ view/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     build: {
         outDir: "public",
         input: {
-            app: "./src/bootstrap.js",
+            app: "./src/bootstrap.ts",
         },
     },
     server: {
