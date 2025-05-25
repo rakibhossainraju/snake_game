430fb7d8f105f110b352848d58d2609cd0326dab
rakibhossainraju <rakibhossianraju@gmail.com>
2025-05-25 22:38:47 +0600

    Refactor CanvasManager to manage world state internally
    Moved world initialization and state updates into CanvasManager, simplifying the canvas setup process by removing the need to pass `worldSize` and `snakeIdx` externally. Introduced `startGame` to handle game rendering and snake updates within the class. This improves encapsulation and centralizes game-related logic.

--- view/src/main.js
+++ view/src/main.js
@@ -1,8 +1,3 @@
-import { World } from "snake_game";
 import { canvasManager } from "./services/CanvasManager.js";
 
-const world = World.new();
-const worldSize = world.get_width();
-const snakeIdx = world.get_snake_head_idx();
-
-canvasManager.initCanvas(worldSize, snakeIdx);
+canvasManager.initCanvas();
--- view/src/services/CanvasManager.js
+++ view/src/services/CanvasManager.js
@@ -1,46 +1,54 @@
+import { World } from "snake_game";
+
 class CanvasManager {
   #CELL_SIZE = 25;
   #canvas = null;
   #ctx = null;
+  #world;
+  #worldSize;
 
   constructor() {
     if (CanvasManager.instance) {
       return CanvasManager.instance;
     }
-
+    this.#world = World.new();
+    this.#worldSize = this.#world.get_width();
     this.#canvas = document.getElementById("snake-canvas");
     this.#ctx = this.#canvas.getContext("2d");
 
     CanvasManager.instance = this;
   }
 
-  initCanvas(worldSize, snakeIdx) {
-    if (!worldSize || !snakeIdx) {
-      throw new Error("Canvas must be invoked with worldSize and snakeIdx");
-    }
-    this.worldSize = worldSize;
-    this.snakeIdx = snakeIdx;
-    this.#canvas.height = this.#CELL_SIZE * worldSize;
-    this.#canvas.width = this.#CELL_SIZE * worldSize;
-    this.drawWorld();
-    this.drawSnake();
+  initCanvas() {
+    this.#canvas.height = this.#CELL_SIZE * this.#worldSize;
+    this.#canvas.width = this.#CELL_SIZE * this.#worldSize;
+    this.startGame();
+  }
+
+  startGame() {
+    const intervelId = setInterval(() => {
+      this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
+      this.drawWorld();
+      this.drawSnake();
+      this.#world.update_snake_head();
+    }, 100);
   }
 
   drawWorld() {
     this.#ctx.beginPath();
 
-    for (let line = 0; line <= this.worldSize; line++) {
+    for (let line = 0; line <= this.#worldSize; line++) {
       // Draw Horizontal Lines
       this.#ctx.moveTo(this.#CELL_SIZE * line, 0);
       this.#ctx.lineTo(
         this.#CELL_SIZE * line,
-        this.worldSize * this.#CELL_SIZE,
+        this.#worldSize * this.#CELL_SIZE,
       );
 
       // Draw Vertical Lines
       this.#ctx.moveTo(0, this.#CELL_SIZE * line);
       this.#ctx.lineTo(
-        this.#CELL_SIZE * this.worldSize,
+        this.#CELL_SIZE * this.#worldSize,
         this.#CELL_SIZE * line,
       );
     }
@@ -49,8 +57,9 @@ class CanvasManager {
   }
 
   drawSnake() {
-    const col = this.snakeIdx % this.worldSize;
-    const row = Math.floor(this.snakeIdx / this.worldSize);
+    const snakeIdx = this.#world.get_snake_head_idx();
+    const col = snakeIdx % this.#worldSize;
+    const row = Math.floor(snakeIdx / this.#worldSize);
 
     this.#ctx.beginPath();
     this.#ctx.fillRect(
