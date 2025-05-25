ca8e72cea5434056e9816d47a296184242cc3875
rakibhossainraju <rakibhossianraju@gmail.com>
2025-05-25 21:17:03 +0600

    Update canvas initialization to include snake index
    Modified `initCanvas` to accept the snake head index as a parameter. This ensures the canvas setup now considers the snake's initial position.

--- view/src/main.js
+++ view/src/main.js
@@ -3,5 +3,6 @@ import { canvasManager } from "./services/CanvasManager.js";
 
 const world = World.new();
 const worldSize = world.get_width();
+const snakeIdx = world.get_snake_head_idx();
 
-canvasManager.initCanvas(worldSize);
+canvasManager.initCanvas(worldSize, snakeIdx);
